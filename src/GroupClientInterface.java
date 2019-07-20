de724820ab9ca995d3ed28b78eecf6f9a07c0cce
Sean Mizerski <stm107@pitt.edu>
2019-07-20 18:33:29 -0400

    .

--- src/GroupClientInterface.java
+++ src/GroupClientInterface.java
@@ -47,7 +47,7 @@ public interface GroupClientInterface {
      * @return true if user exists, false if it does not.
      *
      */
-    public boolean userExists(final String username);
+    public boolean userExists(final String username, final String fip, final int fport);
 
     /**
      * Method used to determine if the user's password is correct.
